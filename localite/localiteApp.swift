094277b0c3a52f5ecd8c78b64526b69e1711bf6d
Anoop <arthavgonda@gmail.com>
2026-07-18 15:06:02 +0530

    Initial Commit

---
+++ localite/localiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  localiteApp.swift
+//  localite
+//
+//  Created by ANOOP on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct localiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
